d1d3be86bb9817762c557333d6dba4a767ba1310
Agnieszka Nowacka <agnesnowacka1@gmail.com>
2023-04-18 12:54:16 +0200

    created Portfolio component with styles

---
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/index.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { SubHeader } from "../SubHeader";
+import { githubUsername } from "./githubUsername";
+import { Section, Header, StyledGithubIcon, MyRecentProjects } from "./styled";
+import { fetchRepositories, selectRepositories, selectRepositoriesStatus } from "../../personalHomepageSlice";
+
+
+export const Portfolio = () => {
+    const dispatch = useDispatch();
+
+    const repositoriesStatus = useSelector(selectRepositoriesStatus);
+    const repositories = useSelector(selectRepositories);
+
+    useEffect(() => {
+        dispatch(fetchRepositories(githubUsername));
+    }, [dispatch]);
+
+    return (
+        <Section>
+            <Header>
+                <StyledGithubIcon />
+                <SubHeader>Portfolio</SubHeader>
+                <MyRecentProjects>My recent projects</MyRecentProjects>
+            </Header>
+        </Section>
+    );
+};
