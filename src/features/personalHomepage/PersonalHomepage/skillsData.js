d41bda487ddf589a75153491c06f7e4122082c28
Agnieszka Nowacka <agnesnowacka1@gmail.com>
2023-04-18 22:02:49 +0200

    added skillsData

---
+++ src/features/personalHomepage/PersonalHomepage/skillsData.js
@@ -0,0 +1,29 @@
+export const skills = [
+    "Semantic & accessible HTML",
+    "Javascript ES6+",
+    "React",
+    "Git",
+    "Error handling",
+    "Responsive Web Design",
+    "Redux (Toolkit)",
+    "Redux-Saga",
+    "Working with API (fetch, axios)",
+    "Immutability",
+    "Promises, Async/Await",
+    "CSS BEM convention",
+    "CSS Flexbox",
+    "CSS Grid",
+    "React Router",
+    "NPM",
+    "React Hooks",
+    "Teamwork",
+    
+];
+
+export const nextSkills = [
+    "TypeScript",
+    "React Context",
+    "Node.js",
+    "JS classes",
+
+];
